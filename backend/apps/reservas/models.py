aaa82fbf1821c54514ff6a5d7c5a0de634b4d992
valen <valensenefacultad@gmail.com>
2026-05-15 01:11:13 -0300

    Avances sobre la funcionalidad de cancelación de reservas

--- backend/apps/reservas/models.py
+++ backend/apps/reservas/models.py
@@ -1,25 +1,55 @@
 from django.db import models
 from apps.usuarios.models import Cliente
-# Importamos el modelo de clases/sesiones que está haciendo tu compañero
-# from clases.models import Sesion 
+
 
 class Reserva(models.Model):
+
     ESTADOS = (
         ('CONFIRMADA', 'Confirmada'),
         ('PENDIENTE', 'Pendiente de Pago'),
         ('CANCELADA', 'Cancelada'),
     )
-    
-    paciente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    # sesion = models.ForeignKey('clases.Sesion', on_delete=models.CASCADE)
+
+    paciente = models.ForeignKey(
+        Cliente,
+        on_delete=models.CASCADE
+    )
+
+    clase = models.ForeignKey(
+        'clases.Clase',
+        on_delete=models.CASCADE,
+        related_name='reservas'
+    )
+
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    estado = models.CharField(max_length=20, choices=ESTADOS, default='PENDIENTE')
-    asistio = models.BooleanField(default=False) # Para la HU de marcar con QR
+
+    estado = models.CharField(
+        max_length=20,
+        choices=ESTADOS,
+        default='PENDIENTE'
+    )
+
+    asistio = models.BooleanField(default=False)
+
 
 class ListaEspera(models.Model):
-    paciente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    # sesion = models.ForeignKey('clases.Sesion', on_delete=models.CASCADE)
+
+    paciente = models.ForeignKey(
+        Cliente,
+        on_delete=models.CASCADE
+    )
+
+    clase = models.ForeignKey(
+        'clases.Clase',
+        on_delete=models.CASCADE,
+        related_name='lista_espera'
+    )
+
     fecha_inscripcion = models.DateTimeField(auto_now_add=True)
+
     notificado = models.BooleanField(default=False)
-    # Para la regla de las 2 horas de Laura
-    fecha_notificacion = models.DateTimeField(null=True, blank=True)
+
+    fecha_notificacion = models.DateTimeField(
+        null=True,
+        blank=True
+    )
