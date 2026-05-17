0ad9007904f694076245e8441fa5be20e48fcbc7
jonashariyo72 <jonashariyo70@gmail.com>
2026-05-17 17:55:42 -0300

    Resuelvo conflictos

--- backend/apps/reservas/models.py
+++ backend/apps/reservas/models.py
@@ -1,36 +1,25 @@
 from django.db import models
 from apps.usuarios.models import Cliente
-<<<<<<< HEAD
 from apps.clases.models import Clase
-=======
 from django.utils import timezone
->>>>>>> origin/main
 
 
 class Reserva(models.Model):
 
     ESTADOS = (
-        ('CONFIRMADA', 'Confirmada'),
+        ('CONFIRMADA', 'Confirmada'), 
         ('PENDIENTE', 'Pendiente de Pago'),
         ('CANCELADA', 'Cancelada'),
     )
 
     paciente = models.ForeignKey(
         Cliente,
-<<<<<<< HEAD
         on_delete=models.CASCADE,
         related_name='reservas'
     )
 
     clase = models.ForeignKey(
         Clase,
-=======
-        on_delete=models.CASCADE
-    )
-
-    clase = models.ForeignKey(
-        'clases.Clase',
->>>>>>> origin/main
         on_delete=models.CASCADE,
         related_name='reservas'
     )
@@ -45,10 +34,6 @@ class Reserva(models.Model):
 
     asistio = models.BooleanField(default=False)
 
-<<<<<<< HEAD
-    def __str__(self):
-        return f'{self.paciente} - {self.clase}'
-=======
     fecha_reserva = models.DateTimeField(default=timezone.now)
 
     PAGO_CHOICES = (
@@ -67,7 +52,6 @@ class Reserva(models.Model):
         decimal_places=2,
         default=0
     )
->>>>>>> origin/main
 
 
 class ListaEspera(models.Model):
@@ -78,11 +62,7 @@ class ListaEspera(models.Model):
     )
 
     clase = models.ForeignKey(
-<<<<<<< HEAD
         Clase,
-=======
-        'clases.Clase',
->>>>>>> origin/main
         on_delete=models.CASCADE,
         related_name='lista_espera'
     )
@@ -91,7 +71,6 @@ class ListaEspera(models.Model):
 
     notificado = models.BooleanField(default=False)
 
-<<<<<<< HEAD
     # Para la regla de las 2 horas de Laura
     fecha_notificacion = models.DateTimeField(
         null=True,
@@ -100,9 +79,7 @@ class ListaEspera(models.Model):
 
     def __str__(self):
         return f'{self.paciente} - {self.clase}'
-=======
     fecha_notificacion = models.DateTimeField(
         null=True,
         blank=True
     )
->>>>>>> origin/main
