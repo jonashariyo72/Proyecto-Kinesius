5389837a9cc871a6ffe19b717e1c86a232dae9d4
MatiasFischer <matiasafischer@hotmail.com>
2026-05-27 19:04:27 -0300

    Cosas esteticas y de creacion de clases

--- backend/apps/clases/models.py
+++ backend/apps/clases/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from apps.usuarios.models import Kinesiologo
 
 
@@ -20,7 +21,7 @@ class Clase(models.Model):
     tipo             = models.CharField(max_length=50, choices=TIPO_CHOICES)
     descripcion      = models.TextField(blank=True, null=True)
     dia              = models.CharField(max_length=20, choices=DIA_CHOICES)
-    fecha_clase = models.DateField(null=True, blank=True)
+    fecha_clase      = models.DateField(null=True, blank=True)
     hora_inicio      = models.TimeField()
     duracion_minutos = models.PositiveIntegerField(default=60)
     capacidad_maxima = models.PositiveIntegerField()
@@ -28,7 +29,6 @@ class Clase(models.Model):
     activa           = models.BooleanField(default=True)
     sala             = models.PositiveIntegerField(null=True, blank=True)
 
-    # Máximo 3 kinesiólogos por turno
     kinesiologo = models.ForeignKey(
         Kinesiologo,
         on_delete=models.SET_NULL,
@@ -39,21 +39,51 @@ class Clase(models.Model):
 
     class Meta:
         verbose_name = 'Clase'
-
-        # Una misma combinación de día + hora + kinesiólogo no puede repetirse
-        unique_together = ('dia', 'hora_inicio', 'kinesiologo')
-
         ordering = ['dia', 'hora_inicio']
 
     def __str__(self):
         return f'{self.get_tipo_display()} - {self.get_dia_display()} {self.hora_inicio}'
 
+    def clean(self):
+
+        # Validar misma sala, mismo día y horario
+        conflicto_sala = Clase.objects.filter(
+            dia=self.dia,
+            hora_inicio=self.hora_inicio,
+            sala=self.sala,
+            activa=True
+        ).exclude(id=self.id)
+
+        if conflicto_sala.exists():
+            raise ValidationError({
+                'sala': 'Ya existe una clase en esa sala, día y horario.'
+            })
+
+        # Validar mismo kinesiólogo, mismo día y horario
+        conflicto_kinesiologo = Clase.objects.filter(
+            dia=self.dia,
+            hora_inicio=self.hora_inicio,
+            kinesiologo=self.kinesiologo,
+            activa=True
+        ).exclude(id=self.id)
+
+        if conflicto_kinesiologo.exists():
+            raise ValidationError({
+                'kinesiologo': 'El kinesiólogo ya tiene una clase asignada en ese horario.'
+            })
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     def cupos_disponibles(self):
         from apps.reservas.models import Reserva
+
         reservas_activas = Reserva.objects.filter(
             clase=self,
             estado='CONFIRMADA'
         ).count()
+
         return self.capacidad_maxima - reservas_activas
 
     def tiene_cupo(self):
